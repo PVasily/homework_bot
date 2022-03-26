87dacfc35c6a2fa45135a2af43b21888d903e970
PVasily <p.vasily81@yandex.ru>
2022-03-26 21:16:41 +0300

    Next commit

---
+++ exceptions.py
@@ -0,0 +1,36 @@
+class EmptyResponse(Exception):
+    """Обрабатываем пустой ответ."""
+
+    def __init__(self, response):
+        """Создаем поле response."""
+        self.response = response
+
+    def __str__(self):
+        """Формируем сообщение об ошибке."""
+        if 'homeworks' not in self.response:
+            return 'В ответе отсутствует ключ "homeworks"'
+        return 'В ответе отсутствует ключ "current_date"'
+
+
+class UnreachableTelegram(Exception):
+    """Telegram недоступен."""
+
+    def __init__(self, message):
+        """Создаем поле message."""
+        self.message = message
+
+    def __str__(self):
+        """Формируем сообщение об ошибке."""
+        return f'{self.message}'
+
+
+class UnknownStatus(Exception):
+    """Неизвестный статус."""
+
+    def __init__(self, message):
+        """Создаем поле message."""
+        self.message = message
+
+    def __str__(self):
+        """Формируем сообщение об ошибке."""
+        return f'{self.message}'
--- homework.py
+++ homework.py
@@ -1,53 +1,47 @@
+import sys
+import time
+import http
 import logging
 from logging.handlers import RotatingFileHandler
 
-import os
-import sys
-from dotenv import load_dotenv
-
-import time
-import requests
-import http
-
 import telegram
+import requests
 
-load_dotenv()
-
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.DEBUG,
-    filemode='w',
-    encoding='UTF-8',
-    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
-
-handler = RotatingFileHandler(
-    'my_logger.log',
-    maxBytes=50000000,
-    encoding='UTF-8',
-    backupCount=5)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-handler.setFormatter(formatter)
+from constants import (
+    ENDPOINT,
+    PRACTICUM_TOKEN,
+    TELEGRAM_TOKEN,
+    TELEGRAM_CHAT_ID,
+    RETRY_TIME,
+    HEADERS,
+    HOMEWORK_STATUSES)
+from exceptions import EmptyResponse, UnreachableTelegram, UnknownStatus
 
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+def get_logger():
+    """Конфигурация логгера."""
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='main.log',
+        filemode='w',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
 
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    handler = RotatingFileHandler(
+        'my_logger.log',
+        maxBytes=50000000,
+        encoding='UTF-8',
+        backupCount=5)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    return logger
 
 
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
+logger = get_logger()
 
 
 def send_message(bot, message):
@@ -56,6 +50,7 @@ def send_message(bot, message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except telegram.TelegramError:
         logger.error('Сбой при отправке сообщения')
+        raise UnreachableTelegram('В данный момент Telegram недоступен')
     logger.info(f'Отправка сообщения: {message}')
 
 
@@ -71,30 +66,27 @@ def get_api_answer(current_timestamp):
             return result
         except Exception as error:
             logger.error(error, exc_info=True)
-    elif response.status_code in (500, 599):
-        logger.error('Код статуса 500')
+    elif response.status_code in (
+            http.HTTPStatus.INTERNAL_SERVER_ERROR,
+            http.HTTPStatus.NETWORK_AUTHENTICATION_REQUIRED):
+        logger.error('Код статуса в диапазоне 500')
         raise requests.exceptions.ConnectionError
     elif response.status_code == http.HTTPStatus.REQUEST_TIMEOUT:
         logger.error('Код статуса 408')
         raise requests.exceptions.ReadTimeout
-    else:
-        logger.error(f'Сбой в работе программы: Эндпоинт'
-                     f'{ENDPOINT} недоступен.'
-                     f'Код ответа API: {response.status_code}')
-        return result
+    logger.error(f'Сбой в работе программы: Эндпоинт'
+                 f'{ENDPOINT} недоступен.'
+                 f'Код ответа API: {response.status_code}')
 
 
 def check_response(response):
-    """ПРоверяем ответ на соответствие типам данных Python."""
-    if type(response) != dict:
+    """Проверяем ответ на соответствие типам данных Python."""
+    if not isinstance(response, dict):
         logger.error('Тип ответа не словарь')
         raise TypeError('Тип ответа не словарь')
-    elif 'homeworks' not in response:
-        logger.error('Отсутствует ключ "homeworks"')
-        raise KeyError('Отсутствует ключ "homeworks"')
-    elif 'current_date' not in response:
-        logger.error('Отсутствует ключ "current_date"')
-        raise KeyError('Отсутствует ключ "current_date"')
+    elif 'homeworks' not in response or 'current_date' not in response:
+        logger.error('Отсутствуют ключи "homeworks" или "current_date')
+        raise EmptyResponse(response)
     elif type(response['homeworks']) is not list:
         logger.error('"homeworks" не список')
         raise TypeError('"homeworks" не список')
@@ -103,17 +95,17 @@ def check_response(response):
 
 def parse_status(homework):
     """На основе данных формируем вердикт о статусе работы."""
-    if len(homework) > 0:
-        homework_name = homework['homework_name']
-        homework_status = homework['status']
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    if HOMEWORK_STATUSES[homework_status] is not None:
         verdict = HOMEWORK_STATUSES[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    return []
+    raise UnknownStatus('Неизвестный статус')
 
 
 def check_tokens():
     """Проверяем наличие всех токенов."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
         return True
     return False
 
@@ -124,11 +116,13 @@ def main():
     LAST_ERROR = None
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+    send_message('Бот активирован.')
     while True:
         try:
             check = check_tokens()
             if check:
-                api = get_api_answer(current_timestamp - 900)
+                api = get_api_answer(current_timestamp)
+                current_timestamp = api['current_date']
                 homeworks = check_response(api)
                 if len(homeworks) > 0:
                     status = homeworks[0]['status']
@@ -140,10 +134,9 @@ def main():
                         logger.info('Нет изменений в статусе работы')
                 else:
                     logger.debug('Response is empty')
-                time.sleep(RETRY_TIME)
             else:
                 logger.CRITICAL('There is not any enviroment variable')
-                break
+                sys.exit('Отсутствуют переменные окружения')
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
@@ -151,6 +144,7 @@ def main():
             if LAST_ERROR != error:
                 send_message(bot, message)
                 LAST_ERROR = error
+        finally:
             time.sleep(RETRY_TIME)
 
 
