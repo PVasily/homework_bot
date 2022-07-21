bbd8764a1d28acea756a53354f5c3b9ee6ff8b1a
PVasily <78196602+PVasily@users.noreply.github.com>
2022-07-21 14:28:20 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 import os
 
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
